cf9870d775108f6876f99aad072dae61db9aece3
OviRei <wasimchy100@gmail.com>
2021-03-10 22:04:28 +0000

    Refrac + smol changes

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-const ColorNames = ["Red", "Yellow", "Green", "Blue", "Cyan", "Purple"];
-const ColorHex = ["#ff073a", "#FFFF00", "#00FF00", "#0000ff", "#00FFFF", "#800080"];
-let ColorRandomNum;
-let HexRandomNum;
-let equal = false;
-let different = 0;
-let click = 0;
+//Variable Initialising
+const colorNames = ["Red", "Yellow", "Green", "Blue", "Cyan", "Purple"];
+const colorHex = ["#ff073a", "#FFFF00", "#00FF00", "#0000ff", "#00FFFF", "#800080"];
 let score = -1;
 let highscore = -1;
+let clicks = 0;
 
-document.oncontextmenu = rightClick; 
-
-function simulateEvent(chances) {
-    var sum = 0;
-    chances.forEach(function(chance) {
-        sum+=chance;
+// Generates the chances for colors displayed
+function simulateEvent(chances) 
+{
+    let sum = 0;
+    chances.forEach(function(chance) 
+    {
+        sum += chance;
     });
-    var rand = Math.random();
-    var chance = 0;
-    for(var i=0; i<chances.length; i++) {
-        chance+=chances[i]/sum;
-        if(rand<chance) {
+    let rand = Math.random();
+    let chance = 0;
+    for(var i = 0; i < chances.length; i++) 
+    {
+        chance += chances[i] / sum;
+        if(rand<chance) 
+        {
             return i;
         }
     }
     return -1;
 }
 
-function Start() {
-    let colorNamelikelihoods;
-    let colorHexlikelihoods;
-    if(different >= 6)
+//Variable Initialising
+let colorDifferences = 0; //Counts the ammount of times the hex color and text color are different
+let colorRandomNum;
+let hexRandomNum;
+let colorNamelikelihoods;
+let colorHexlikelihoods;
+let equalColors = false;
+
+//Changes colors
+function changeColor() 
+{
+    //Changes the likelyhoods of hex colors and text colors being either the same or different
+    if(colorDifferences >= 6)
     {
         colorNamelikelihoods = [5,5,5,5,5,5];
         colorHexlikelihoods = [5,5,5,5,5,5];
@@ -40,58 +49,70 @@ function Start() {
         colorHexlikelihoods = [1,3,2,5,3,2]; 
     }
 
+    //Likelyhood function
+    colorRandomNum = simulateEvent(colorHexlikelihoods);
+    hexRandomNum = simulateEvent(colorNamelikelihoods);
 
-    ColorRandomNum = simulateEvent(colorHexlikelihoods);
-    HexRandomNum = simulateEvent(colorNamelikelihoods);
-    score++;
-
-    document.getElementById("StartText").innerHTML = ColorNames[ColorRandomNum];
-    document.getElementById("StartText").style.color = ColorHex[HexRandomNum]; 
-    document.getElementById("Score").innerHTML = `Score: ${score}`;
-
-    if(ColorRandomNum == HexRandomNum) 
+    //Checks if the hex color and text color are the same
+    if(colorRandomNum == hexRandomNum) 
     {
-        equal = true; 
-        different=0;
+        equalColors = true; 
+        colorDifferences = 0;
     }
     else 
     {
-        equal = false; 
-        different++;
+        equalColors = false;
+        colorDifferences++;
     }
-};
 
-function GameOver() {
-    if(click > 1)
+    //Add onto the score
+    score++;
+
+    //Changes the hex color and text color + updates score
+    document.getElementById("ColorText").innerHTML = colorNames[colorRandomNum];
+    document.getElementById("ColorText").style.color = colorHex[hexRandomNum]; 
+    document.getElementById("Score").innerHTML = `Score: ${score}`;
+}
+
+//Game over :p
+function gameOver() {
+    if(clicks > 1)
     {
+        //Sets highscore and resets game
         if(score >= highscore)
         {
+            //TODO Use localstorage to store highscore
             highscore = score;
             document.getElementById("Highscore").innerHTML = `Highscore: ${highscore}`;
         }
-        document.getElementById("StartText").innerHTML = "Game Over";
-        document.getElementById("StartText").style.color = "black"; 
+        document.getElementById("ColorText").innerHTML = "Game Over";
+        document.getElementById("ColorText").style.color = "white"; 
         document.getElementById("Score").innerHTML = `Score: ${score}`;
 
-        click = 0;
+        clicks = 0;
         score = 0;
     }
     else
     {
-        Start();
+        changeColor();
     }
 }
 
-function rightClick(clickEvent) { 
+//Right Click event
+document.oncontextmenu = rightClick; 
+function rightClick(clickEvent) 
+{ 
     clickEvent.preventDefault(); 
 
-    click++;
-    if(!equal) Start();
-    else GameOver();
+    clicks++;
+    if(!equalColors) changeColor();
+    else gameOver();
 };
 
-document.body.addEventListener("click", function () {
-    click++;
-    if(equal) Start();
-    else GameOver();
+//Left Click event
+document.body.addEventListener("click", function () 
+{
+    clicks++;
+    if(equalColors) changeColor();
+    else gameOver();
 });
